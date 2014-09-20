689c7e3647df42dac19e411a4891adaaf277cbfd
cliftonc <clifton.cunningham@gmail.com>
2014-09-20 21:28:02 +0100

    Fix bug if replace-outer and so lose cx-url attribute

--- src/client/cx-stats.js
+++ src/client/cx-stats.js
@@ -8,14 +8,17 @@ function renderStats() {
 	for(key in cxStats) {
 		var selector = '[cx-url="' + key + '"]';
 		var fragment = document.querySelector(selector);
-		tooltips.push({
-			contentText: cxHtml[key],
-			targetSelector: selector,
-			color: getColor(cxStats[key].status)
-		});
-		fragment.classList.add('cx-stats-fragment-' + cxStats[key].status);
+		if(fragment) {
+			var stick = fragment.offsetLeft < 500 ? "right" : "left";
+			tooltips.push({
+				contentText: cxHtml[key],
+				targetSelector: selector,
+				color: getColor(cxStats[key].status),
+				stickTo: stick
+			});
+			fragment.classList.add('cx-stats-fragment-' + cxStats[key].status);
+		}
 	}
-
 
 	html5tooltips(tooltips);
 }
