1e9b62754c507046cccb661e216f2437fc709abb
cliftonc <clifton.cunningham@gmail.com>
2014-11-07 20:59:17 +0000

    Fixed bug for invalid links breaking core decodeUri

--- index.js
+++ index.js
@@ -210,6 +210,16 @@ module.exports = function(config, eventHandler) {
 
   }
 
+  function cleanInvalidUri(req, res, next) {
+    try {
+      decodeURI(req.url);
+    } catch(ex) {
+      eventHandler.logger('warn','Filtered out invalid URL - removed all query params.', {invalidUrl: req.url});
+      req.url = req.url.split('?')[0]; // Just take the good parts for now - wtf
+    }
+    next();
+  }
+
   function interrogateRequest(req, res, next) {
     interrogator.interrogateRequest(req, function(templateVars) {
       req.templateVars = templateVars;
@@ -218,6 +228,7 @@ module.exports = function(config, eventHandler) {
   }
 
   var middleware = ware()
+                    .use(cleanInvalidUri)
                     .use(dropFavicon)
                     .use(cacheMiddleware)
                     .use(interrogateRequest)
