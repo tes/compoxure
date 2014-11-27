8c2fc66f8920abde6b62d90e9fbe423304c043d1
cliftonc <clifton.cunningham@gmail.com>
2014-11-27 14:13:36 +0000

    Update test as I hadnt pulled latest deps

--- test/acceptance/compoxure.test.js
+++ test/acceptance/compoxure.test.js
@@ -296,7 +296,7 @@ describe("Page Composer", function(){
             var $ = cheerio.load(response.body);
             var bundles = $('.bundle');
             expect($(bundles[0]).text()).to.be('service-one >> 100 >> top.js.html');
-            expect($(bundles[1]).text()).to.be('service-two >> default >> top.js.html');
+            expect($(bundles[1]).text()).to.be('service-two >> local >> top.js.html');
             done();
         });
     });
