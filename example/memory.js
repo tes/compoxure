d515f8e7f1355aabf531fd8344a8d690c508ccf5
cliftonc <clifton.cunningham@gmail.com>
2015-03-12 15:03:41 +0000

    Add missing files

---
+++ example/memory.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+function bytesToSize(bytes) {
+   return Math.round(bytes / Math.pow(1024, 2), 2);
+}
+fs.writeFileSync(__dirname + '/memory.csv', 'rss,total,used,change' + '\n');
+var previousHeap = 0;
+function memoryPerSecond() {
+  var mem = process.memoryUsage();
+  var variance = mem.heapUsed - previousHeap;
+  if(variance < 0) {
+    variance = '-' + bytesToSize(-variance);
+  } else {
+    variance = bytesToSize(variance);
+  }
+  var data = [bytesToSize(mem.rss), bytesToSize(mem.heapTotal), bytesToSize(mem.heapUsed), variance].join(',');
+  fs.appendFileSync(__dirname + '/memory.csv', data + '\n');
+  previousHeap = mem.heapUsed;
+  setTimeout(memoryPerSecond, 1000);
+}
+memoryPerSecond();
